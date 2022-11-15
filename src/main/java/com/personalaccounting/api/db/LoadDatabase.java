168da5a5bbc45d5e0d54006bea64c50dc16320e2
Harun Can Yildirim <eharuncan@gmail.com>
2022-11-15 17:48:56 +0300

    tamamlandı

---
+++ src/main/java/com/personalaccounting/api/db/LoadDatabase.java
@@ -0,0 +1,25 @@
+package com.personalaccounting.api.db;
+
+import com.personalaccounting.api.domain.User;
+import com.personalaccounting.api.domain.UserRepository;
+import com.personalaccounting.api.enums.UserTypes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new User(UserTypes.ADMIN, "admin", "admin", "admin", "admin")));
+            log.info("Preloading " + repository.save(new User(UserTypes.CUSTOMER, "customer1", "customer1", "1", "1")));
+        };
+    }
+}
